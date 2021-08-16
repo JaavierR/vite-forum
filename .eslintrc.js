a08c4245cbde88c207eabd4e36b6fa24e19754ac
Javier Reyes <javier.reys@gmail.com>
2021-08-16 04:55:50 -0400

    feat: config prettier to match std eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
 module.exports = {
+  root: true,
+
   env: {
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+
+  extends: ['eslint:recommended', 'plugin:vue/vue3-essential', 'prettier'],
+
   rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
-};
+}
