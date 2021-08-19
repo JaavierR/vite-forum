5ba5286de7a9cef0c293031e7ab8c8f23f54c321
Javier Reyes <javier.reys@gmail.com>
2021-08-18 21:19:49 -0400

    refactor: add '@' to the alias

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+import path from 'path'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
@@ -5,4 +7,9 @@ import eslintPlugin from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslintPlugin()],
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
 })
