0d9a5e446e1b48bfeaa37325a5908a21465c861a
Javier Reyes <javier.reys@gmail.com>
2021-08-18 21:20:17 -0400

    feat: add Home component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,16 @@
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="state.count++">
-    count is: {{ state.count }}
-  </button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <h1>Hola Mundo</h1>
+  <div>{{ threads }}</div>
 </template>
 
-<script setup>
-import { defineProps, reactive } from "vue";
+<script>
+import sourceData from '@/data.json'
 
-defineProps({
-  msg: String,
-});
-
-const state = reactive({ count: 0 });
-</script>
-
-<style scoped>
-a {
-  color: #42b983;
+export default {
+  setup() {
+    return {
+      threads: sourceData.threads,
+    }
+  },
 }
-</style>
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>Hello from page home component.</div>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{ userById(postById(postId).userId).name }}</p>
+      <p>{{ postById(postId).text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+export default {
+  setup() {
+    const { threads, posts, users } = sourceData
+
+    const postById = (postId) => {
+      return posts.find((p) => p.id === postId)
+    }
+
+    const userById = (userId) => {
+      return users.find((u) => u.id === userId)
+    }
+
+    return {
+      threads,
+
+      postById,
+      userById,
+    }
+  },
+}
+</script>
