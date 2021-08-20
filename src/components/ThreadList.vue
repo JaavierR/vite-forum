8f43e7b58100ee15281f3ab02315582205d5f215
Javier Reyes <javier.reys@gmail.com>
2021-08-19 22:15:07 -0400

    feat: 🪖 add thread list component & clean up the home component

---
+++ src/components/ThreadList.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="col-full">
+    <div class="thread-list">
+      <h2 class="list-title">Threads</h2>
+
+      <div v-for="thread in threads" :key="thread.id" class="thread">
+        <div>
+          <p>
+            <a href="#">{{ thread.title }}</a>
+          </p>
+
+          <p>
+            By <a href="#">{{ userById(thread.userId).name }}</a
+            >, {{ thread.publishedAt }}.
+          </p>
+        </div>
+
+        <div class="activity">
+          <p class="replies-count">{{ thread.posts.length }}</p>
+
+          <img
+            class="avatar-medium"
+            :src="userById(thread.userId).avatar"
+            alt=""
+          />
+
+          <div>
+            <p class="text-xsmall">
+              <a href="#">{{ userById(thread.userId).name }}</a>
+            </p>
+            <p class="text-xsmall text-faded">{{ thread.publishedAt }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="pagination">
+      <button class="btn-circle" disabled="">
+        <i class="fa fa-angle-left"></i>
+      </button>
+      1 of 3
+      <button class="btn-circle">
+        <i class="fa fa-angle-right"></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+  props: {
+    threads: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup() {
+    const { posts, users } = sourceData
+
+    const postById = (postId) => {
+      return posts.find((p) => p.id === postId)
+    }
+
+    const userById = (userId) => {
+      return users.find((u) => u.id === userId)
+    }
+
+    return {
+      postById,
+      userById,
+    }
+  },
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,58 +1,22 @@
 <template>
-  <div v-for="thread in threads" :key="thread.id" class="col-large push-top">
-    <h1>{{ thread.title }}</h1>
-    <div class="post-list"></div>
-    <div v-for="postId in thread.posts" :key="postId" class="post">
-      <div class="user-info">
-        <a href="#" class="user-name">
-          {{ userById(postById(postId).userId).name }}
-        </a>
-
-        <a href="#">
-          <img
-            :src="userById(postById(postId).userId).avatar"
-            alt=""
-            class="avatar-large"
-          />
-        </a>
-
-        <p class="desktop-only text-small">107 posts</p>
-      </div>
-
-      <div class="post-content">
-        <div>
-          <p>
-            {{ postById(postId).text }}
-          </p>
-        </div>
-      </div>
-
-      <div class="post-date text-faded">
-        {{ postById(postId).publishedAt }}
-      </div>
-    </div>
-  </div>
+  <h1>Welcome to the forum</h1>
+  <thread-list :threads="threads" />
 </template>
 
 <script>
 import sourceData from '@/data.json'
+
+import ThreadList from '@/components/ThreadList.vue'
+
 export default {
+  components: {
+    ThreadList,
+  },
   setup() {
-    const { threads, posts, users } = sourceData
-
-    const postById = (postId) => {
-      return posts.find((p) => p.id === postId)
-    }
-
-    const userById = (userId) => {
-      return users.find((u) => u.id === userId)
-    }
+    const { threads } = sourceData
 
     return {
       threads,
-
-      postById,
-      userById,
     }
   },
 }
