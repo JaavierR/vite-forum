14549899221b8feb7c5d9ba44c47b9bed44f41bd
Javier Reyes <javier.reys@gmail.com>
2021-08-19 22:22:08 -0400

    feat: add router link to access the threads

--- src/components/ThreadList.vue
+++ src/components/ThreadList.vue
@@ -6,7 +6,11 @@
       <div v-for="thread in threads" :key="thread.id" class="thread">
         <div>
           <p>
-            <a href="#">{{ thread.title }}</a>
+            <router-link
+              :to="{ name: 'ThreadShow', params: { id: thread.id } }"
+            >
+              {{ thread.title }}
+            </router-link>
           </p>
 
           <p>
