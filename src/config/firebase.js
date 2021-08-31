29f2e55ffee492452bd83daf47dd13fe901a0152
Javier Reyes <javier.reys@gmail.com>
2021-08-31 16:36:53 -0400

    fix(firebase): :bug: change module.exports to export default in the firebase config file.
    Change the export method in the firebase config file, because vite as troubles with module.exports.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
+import { initializeApp } from 'firebase/app'
 import App from '@/App.vue'
+import firebaseConfig from '@/config/firebase'
 import router from '@/router'
 import store from '@/store'
+// Tailwind styles
 import '@/index.css'
-import { initializeApp } from 'firebase/app'
-import firebaseConfig from '@/config/firebase'
 
 initializeApp(firebaseConfig)
 
