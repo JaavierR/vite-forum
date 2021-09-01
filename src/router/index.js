0ff2e9de811759861c130df7fb8543dd267fd7a5
Javier Reyes <javier.reys@gmail.com>
2021-09-01 17:25:49 -0400

    fix: :bug: change source of beforeEnter route & fix firebase imports in store.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import store from '@/store'
-// import { findById } from '@/helpers'
+import sourceData from '@/data.json'
+import { findById } from '@/helpers'
 
 // Views
 import Home from '@/views/Home.vue'
@@ -35,24 +35,24 @@ const routes = [
     name: 'ThreadShow',
     component: ThreadShow,
     props: true,
-    // beforeEnter(to, from, next) {
-    //   // Check if thread exists
-    //   const threadExists = findById(store.state.threads.threads, to.params.id)
-    //   // if exists continue
-    //   if (threadExists) {
-    //     return next()
-    //   } else {
-    //     next({
-    //       name: 'NotFound',
-    //       // preserve current path and remove the first char to avoid the target URL starting with `//`
-    //       params: { pathMatch: to.path.substring(1).split('/') },
-    //       // preserve existing query and hash if any
-    //       query: to.query,
-    //       hash: to.hash,
-    //     })
-    //   }
-    //   // if does not exist redirect to not found
-    // },
+    beforeEnter(to, from, next) {
+      // Check if thread exists
+      const threadExists = findById(sourceData.threads, to.params.id)
+      // if exists continue
+      if (threadExists) {
+        return next()
+      } else {
+        next({
+          name: 'NotFound',
+          // preserve current path and remove the first char to avoid the target URL starting with `//`
+          params: { pathMatch: to.path.substring(1).split('/') },
+          // preserve existing query and hash if any
+          query: to.query,
+          hash: to.hash,
+        })
+      }
+      // if does not exist redirect to not found
+    },
   },
   {
     path: '/forum/:forumId/thread/create',
--- src/store/modules/threads.js
+++ src/store/modules/threads.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase/app'
+import firebase from 'firebase'
 import { findById, makeAppendChildToParentMutation, upsert } from '@/helpers'
 
 export default {
