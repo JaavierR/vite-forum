1d058a14b0f9717953afbb669880734ad76df150
Javier Reyes <javier.reys@gmail.com>
2021-09-03 03:12:42 -0400

    feat: :sparkles: store new post in firebase with vuex

--- src/store/modules/posts.js
+++ src/store/modules/posts.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase/app'
+
 export default {
   namespaced: true,
   state: {
@@ -5,17 +7,37 @@ export default {
   },
   mutations: {},
   actions: {
-    createPost({ commit, rootState }, post) {
-      post.id = 'gggg' + Math.random()
+    async createPost({ commit, rootState }, post) {
       post.userId = rootState.auth.authId
       post.publishedAt = Math.floor(Date.now() / 1000)
+      // * batch allow us to send posts and update the threads in a single
+      // * transaction. This allow us to avoid some errors if the connection is
+      // * lost. lesson 66.
+      const batch = firebase.firestore().batch()
+      const postRef = firebase.firestore().collection('posts').doc()
+      const threadRef = firebase
+        .firestore()
+        .collection('threads')
+        .doc(post.threadId)
 
-      commit('SET_ITEM', { resource: 'posts', item: post }, { root: true })
+      batch.set(postRef, post)
+      batch.update(threadRef, {
+        posts: firebase.firestore.FieldValue.arrayUnion(postRef.id),
+        contributors: firebase.firestore.FieldValue.arrayUnion(
+          rootState.auth.authId
+        ),
+      })
+      await batch.commit()
+      commit(
+        'SET_ITEM',
+        { resource: 'posts', item: { ...post, id: postRef.id } },
+        { root: true }
+      )
       commit(
         'threads/APPEND_POST_TO_THREAD',
         {
           parentId: post.threadId,
-          childId: post.id,
+          childId: postRef.id,
         },
         { root: true }
       )
