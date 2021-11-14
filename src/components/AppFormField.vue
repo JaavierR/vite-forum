935e241854dbb6407fc65c3e0a24cfdcc4ac6b96
Javier Reyes <javier.reys@gmail.com>
2021-11-14 02:33:01 -0300

    refactor: :art: wrap veevalidate field with custom appformfield

---
+++ src/components/AppFormField.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="form-group">
+    <label :for="name">{{ label }}</label>
+    <VeeField
+      :name="name"
+      :modelValue="modelValue"
+      @input="$emit('modelUpdate', $event)"
+      :label="label"
+      :id="name"
+      class="form-input"
+      v-bind="$attrs"
+    />
+    <VeeErrorMessage :name="name" class="form-error" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    modelValue: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
