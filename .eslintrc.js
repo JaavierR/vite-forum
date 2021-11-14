6d9915bed52e6f9cdd0b4a29469b0cc5f10102f9
Javier Reyes <javier.reys@gmail.com>
2021-11-14 02:31:48 -0300

    chore: :rotating_light: remove multiname component error from all project

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,6 @@ module.exports = {
     'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off',
   },
 }
